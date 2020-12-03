c1a6c987beee1cf1dc3fbbab21db27c463c1d52a
gabriel.strano <gabriel.strano@master.hes-so.ch>
2020-12-03 16:42:36 +0100

    add sharpness

--- src/components/Stuff/CurrentSet.vue
+++ src/components/Stuff/CurrentSet.vue
@@ -5,10 +5,9 @@
          <!-- Current Armor Set -->
          <div v-for="piece in myCurrentArmorSet" v-bind:key="piece.id" class="border my-1 mx-2">
             <div
-               v-if="detailArmor !== piece.id"
-               class=""
-               @mouseover="mouseOverId = piece.id"
-              
+               v-if="details !== 'a' + piece.id"
+               @mouseover="mouseOverId = 'a' + piece.id"
+               class="card border-dark"
             >
                <!-- TODO: change by an image -->
                <table>
@@ -51,8 +50,8 @@
                            height="80"
                         />
                      </td>
-                     <td v-if="mouseOverId != piece.id"><b>&#62;</b></td>
-                     <td colspan="2" v-if="mouseOverId == piece.id">
+                     <td v-if="mouseOverId != 'a' + piece.id"><b>&#62;</b></td>
+                     <td colspan="2" v-if="mouseOverId == 'a' + piece.id">
                         <table>
                            <tr>
                               <td>
@@ -135,6 +134,107 @@
     --->
             </div>
          </div>
+         <div v-for="weapon in myCurrentWeapon" v-bind:key="weapon.id" class="border my-1 mx-2">
+            <div
+               v-if="details !== 'w' + weapon.id"
+               @mouseover="mouseOverId = 'w' + weapon.id"
+               class="card border-dark"
+            >
+               <table>
+                  <tr>
+                     <td class="float-left">
+                        <button
+                           type="button"
+                           class="close"
+                           @click="deleteWeaponCard"
+                           aria-label="Close"
+                        >
+                           <span aria-hidden="true">&times;</span>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td colspan="2">{{ weapon.name }}</td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <img
+                           v-if="weapon.assets === null"
+                           :src="getImg(weapon.type)"
+                           :alt="weapon.type"
+                           width="80"
+                           height="80"
+                        />
+                        <img
+                           v-else-if="weapon.assets.image"
+                           :src="weapon.assets.image"
+                           width="80"
+                           height="80"
+                        />
+                     </td>
+                     <td v-if="mouseOverId != 'w' + weapon.id"><b>&#62;</b></td>
+                     <td colspan="2" v-if="mouseOverId == 'w' + weapon.id">
+                        <table>
+                           <tr>
+                              <td>
+                                 <span class="badge badge-primary">attack:</span>
+                                 {{ weapon.attack.display }}
+                              </td>
+                              <td>
+                                 <span
+                                    v-for="lvl in getListRankJewels(weapon.slots)"
+                                    :key="lvl.id"
+                                    class="pr-1"
+                                 >
+                                    <img :src="getImgJewel(lvl.rank)" width="24" height="24"
+                                 /></span>
+                              </td>
+                           </tr>
+                           <tr>
+                              <td>
+                                 <span class="badge badge-primary">elderseal</span>
+                                 <span v-if="weapon.elderseal === null">none</span
+                                 ><span v-else>{{ weapon.elderseal }}</span>
+                              </td>
+
+                              <td>
+                                 <span class="badge badge-primary">elements</span>
+                                 <span v-if="weapon.elements.length === 0">none</span
+                                 ><span v-else>
+                                    <img
+                                       :src="getImg(weapon.elements[0].type)"
+                                       :alt="weapon.elements[0].type"
+                                       width="32"
+                                       height="32"
+                                    />{{ weapon.elements[0].damage }}</span
+                                 >
+                              </td>
+                           </tr>
+                        </table>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td colspan="2">
+                        <div class="progress" style="max-width: 100%; min-width: 100px;"> 
+                       <div class=" progress-bar"  :style="{width: Math.round(weapon.durability[0].red / Object.keys(weapon.durability[0]).length)  +'%','background-color':'red'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(weapon.durability[0].orange/Object.keys(weapon.durability[0]).length)+'%','background-color':'orange'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(weapon.durability[0].yellow/Object.keys(weapon.durability[0]).length)+'%','background-color':'yellow'}"></div>
+                       
+                       <div class="progress-bar" :style="{width: Math.round(weapon.durability[0].green/Object.keys(weapon.durability[0]).length)+'%','background-color':'green'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(weapon.durability[0].blue/Object.keys(weapon.durability[0]).length)+'%','background-color':'blue'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(weapon.durability[0].white/Object.keys(weapon.durability[0]).length)+'%','background-color':'white'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(weapon.durability[0].purple/Object.keys(weapon.durability[0]).length)+'%','background-color':'purple'}"></div>
+                        </div>
+                     </td>
+                  </tr>
+               </table>
+            </div>
+         </div>
       </div>
    </div>
 </template>
@@ -164,7 +264,7 @@ export default {
    data() {
       return {
          mouseOverId: "-1",
-         detailArmor: "-1",
+         details: "-1",
          detailWeapon: false,
       }
    },
@@ -180,12 +280,12 @@ export default {
       },
       showArmorDetails(id) {
          console.log("DSK")
-         this.detailArmor = id
+         this.details = id
          this.detailWeapon = false
       },
       showWeaponDetails() {
          console.log("DSK")
-         this.detailArmor = "-1"
+         this.details = "-1"
          this.detailWeapon = true
       },
       getImgEmpty(name) {
--- src/components/Stuff/WeaponCard.vue
+++ src/components/Stuff/WeaponCard.vue
@@ -5,6 +5,10 @@
       style="max-width: 16rem"
       v-bind:class="{ 'border-dark': !isSelected, 'border-success': isSelected }"
    >
+      <div
+         class="card-footer"
+         v-bind:class="{ 'border-dark': !isSelected, 'bg-success': isSelected }"
+      ></div>
       <div>
          <img
             v-if="piece.assets === null"
@@ -23,11 +27,23 @@
          :piece="piece"
          :displayOptions="['type', 'rarity', 'jewels', 'attack', 'elderseal', 'elements']"
       />
+       <div class="progress" style="max-width: 100%; min-width: 100px;"> 
+                       <div class=" progress-bar"  :style="{width: Math.round(piece.durability[0].red / Object.keys(piece.durability[0]).length)  +'%','background-color':'red'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(piece.durability[0].orange/Object.keys(piece.durability[0]).length)+'%','background-color':'orange'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(piece.durability[0].yellow/Object.keys(piece.durability[0]).length)+'%','background-color':'yellow'}"></div>
+                       
+                       <div class="progress-bar" :style="{width: Math.round(piece.durability[0].green/Object.keys(piece.durability[0]).length)+'%','background-color':'green'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(piece.durability[0].blue/Object.keys(piece.durability[0]).length)+'%','background-color':'blue'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(piece.durability[0].white/Object.keys(piece.durability[0]).length)+'%','background-color':'white'}"></div>
+                       
+                       <div class="progress-bar" :style="{width:  Math.round(piece.durability[0].purple/Object.keys(piece.durability[0]).length)+'%','background-color':'purple'}"></div>
+                        </div>
 
-      <div
-         class="card-footer"
-         v-bind:class="{ 'border-dark': !isSelected, 'bg-success': isSelected }"
-      ></div>
+   
    </div>
 </template>
 
